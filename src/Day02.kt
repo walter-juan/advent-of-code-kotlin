59d104833eb027718f9047208433adf472602e0c
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2022-12-02 20:35:33 +0100

    Day 2 - part 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -2,15 +2,23 @@
 enum class Shape(val score: Int) {
     ROCK(1) {
         override fun win(shape: Shape): Boolean = shape == SCISSORS
+        override fun lose(shape: Shape): Boolean = shape == PAPER
+        override fun draw(shape: Shape): Boolean = shape == ROCK
     },
     PAPER(2) {
         override fun win(shape: Shape): Boolean = shape == ROCK
+        override fun lose(shape: Shape): Boolean = shape == SCISSORS
+        override fun draw(shape: Shape): Boolean = shape == PAPER
     },
     SCISSORS(3) {
         override fun win(shape: Shape): Boolean = shape == PAPER
+        override fun lose(shape: Shape): Boolean = shape == ROCK
+        override fun draw(shape: Shape): Boolean = shape == SCISSORS
     };
 
     abstract fun win(shape: Shape): Boolean
+    abstract fun lose(shape: Shape): Boolean
+    abstract fun draw(shape: Shape): Boolean
 
     companion object {
         fun from(str: String) = if(listOf("A", "X").contains(str)) {
@@ -45,12 +53,26 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val scores = input.map { line ->
+            val shapes = line.split(" ")
+            val opponentShape = Shape.from(shapes[0])
+            val roundEnd = shapes[1]
+
+            if (roundEnd == "X") {
+                Shape.values().first { it.lose(opponentShape) }.score
+            } else if (roundEnd == "Y") {
+                3 + Shape.values().first { it.draw(opponentShape) }.score
+            } else {
+                6 + Shape.values().first { it.win(opponentShape) }.score
+            }
+        }
+
+        return scores.sum()
     }
 
     val testInput = readInput("Day02_test")
     check(part1(testInput) == 15)
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 12)
 
     val input = readInput("Day02")
     println(part1(input))
