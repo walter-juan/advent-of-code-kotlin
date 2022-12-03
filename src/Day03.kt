c14e0a812a2a11dcc8a4863fb893603e14782519
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2022-12-03 12:01:10 +0100

    Day 3 - part 2

--- src/Day03.kt
+++ src/Day03.kt
@@ -1,3 +1,4 @@
+import kotlin.math.ceil
 
 fun main() {
     fun part1(input: List<String>): Int {
@@ -18,14 +19,36 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 1
+        val lowercaseRange = 'a'..'z'
+        val uppercaseRange = 'A'..'Z'
+
+        return input
+            .asSequence()
+            .withIndex()
+            .groupBy({
+                ceil((it.index + 1) / 3.0)
+            }, {
+                it.value
+            })
+            .map { it.value }
+            .mapNotNull {
+                it[0].toList().intersect(it[1].toList()).intersect(it[2].toList()).firstOrNull()
+            }.sumOf { char ->
+                if (lowercaseRange.contains(char)) {
+                    lowercaseRange.indexOf(char) + 1
+                } else if (uppercaseRange.contains(char)) {
+                    uppercaseRange.indexOf(char) + 1 + 26
+                } else {
+                    0
+                }
+            }
     }
 
     val testInput = readInput("Day03_test")
     check(part1(testInput) == 157)
-//    check(part2(testInput) == 1)
+    check(part2(testInput) == 70)
 
     val input = readInput("Day03")
     println(part1(input))
-//    println(part2(input))
+    println(part2(input))
 }
