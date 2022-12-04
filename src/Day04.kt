c4aa5abfd2942aefaed4600bbffce7928c498341
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2022-12-04 10:29:54 +0100

    Day 4 update

--- src/Day04.kt
+++ src/Day04.kt
@@ -2,13 +2,15 @@
 fun main() {
     fun part1(input: List<String>): Int {
         return input.map { line ->
-            val rangesStr = line.split(",")
-            val firstRangeStr = rangesStr[0].split("-")
-            val firstRange = firstRangeStr[0].toInt()..firstRangeStr[1].toInt()
-            val secondRangeStr = rangesStr[1].split("-")
-            val secondRange = secondRangeStr[0].toInt()..secondRangeStr[1].toInt()
+            val ranges = line
+                .split(",")
+                .map { rangeStr ->
+                    rangeStr.split("-").map { it.toInt() }
+                }.map {
+                    it[0]..it[1]
+                }
 
-            if ((firstRange - secondRange).isEmpty() || (secondRange - firstRange).isEmpty()) {
+            if ((ranges[0] - ranges[1]).isEmpty() || (ranges[1] - ranges[0]).isEmpty()) {
                 1
             } else {
                 0
@@ -18,13 +20,15 @@ fun main() {
 
     fun part2(input: List<String>): Int {
         return input.map { line ->
-            val rangesStr = line.split(",")
-            val firstRangeStr = rangesStr[0].split("-")
-            val firstRange = firstRangeStr[0].toInt()..firstRangeStr[1].toInt()
-            val secondRangeStr = rangesStr[1].split("-")
-            val secondRange = secondRangeStr[0].toInt()..secondRangeStr[1].toInt()
+            val ranges = line
+                .split(",")
+                .map { rangeStr ->
+                    rangeStr.split("-").map { it.toInt() }
+                }.map {
+                    it[0]..it[1]
+                }
 
-            if ((firstRange - secondRange).size != firstRange.toList().size || (secondRange - firstRange).size != secondRange.toList().size) {
+            if (ranges[0].intersect(ranges[1]).isNotEmpty()) {
                 1
             } else {
                 0
