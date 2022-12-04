ee4009f4d60418e19e6380f24c7293491899fed5
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2022-12-04 10:00:13 +0100

    Day 4 - part 1

---
+++ src/Day04.kt
@@ -0,0 +1,30 @@
+
+fun main() {
+    fun part1(input: List<String>): Int {
+        return input.map { line ->
+            val rangesStr = line.split(",")
+            val firstRangeStr = rangesStr[0].split("-")
+            val firstRange = firstRangeStr[0].toInt()..firstRangeStr[1].toInt()
+            val secondRangeStr = rangesStr[1].split("-")
+            val secondRange = secondRangeStr[0].toInt()..secondRangeStr[1].toInt()
+
+            if ((firstRange - secondRange).isEmpty() || (secondRange - firstRange).isEmpty()) {
+                1
+            } else {
+                0
+            }
+        }.sum()
+    }
+
+    fun part2(input: List<String>): Int {
+        return 1
+    }
+
+    val testInput = readInput("Day04_test")
+    check(part1(testInput) == 2)
+    check(part2(testInput) == 1)
+
+    val input = readInput("Day04")
+    println(part1(input))
+    println(part2(input))
+}
