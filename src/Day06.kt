7bb792b8b64fc0b3b310125c6719077822b3d75e
Walter Juan <4141614+walter-juan@users.noreply.github.com>
2022-12-11 21:27:18 +0100

    Day 6 - part 2

--- src/Day06.kt
+++ src/Day06.kt
@@ -3,15 +3,16 @@ fun main() {
     fun part1(input: String): Int {
         val chars = mutableListOf<Char>()
         var index = 0
+        val nDistinctChars = 4
         for(char in input) {
             index++
             chars.add(char)
 
-            if (chars.size > 4) {
+            if (chars.size > nDistinctChars) {
                 chars.removeAt(0)
             }
 
-            if (chars.size == 4 && chars.size == chars.distinct().size) {
+            if (chars.size == nDistinctChars && chars.size == chars.distinct().size) {
                 // found it!
                 break
             }
@@ -20,8 +21,25 @@ fun main() {
         return index
     }
 
-    fun part2(input: List<String>): Int {
-        return 1
+    fun part2(input: String): Int {
+        val chars = mutableListOf<Char>()
+        var index = 0
+        val nDistinctChars = 14
+        for(char in input) {
+            index++
+            chars.add(char)
+
+            if (chars.size > nDistinctChars) {
+                chars.removeAt(0)
+            }
+
+            if (chars.size == nDistinctChars && chars.size == chars.distinct().size) {
+                // found it!
+                break
+            }
+        }
+
+        return index
     }
 
     val testInput = readInput("Day06_test")
@@ -30,9 +48,14 @@ fun main() {
     check(part1(testInput[2]) == 6)
     check(part1(testInput[3]) == 10)
     check(part1(testInput[4]) == 11)
-    check(part2(testInput) == 1)
+
+    check(part2(testInput[0]) == 19)
+    check(part2(testInput[1]) == 23)
+    check(part2(testInput[2]) == 23)
+    check(part2(testInput[3]) == 29)
+    check(part2(testInput[4]) == 26)
 
     val input = readInput("Day06")
     println(part1(input[0]))
-    println(part2(input))
+    println(part2(input[0]))
 }
